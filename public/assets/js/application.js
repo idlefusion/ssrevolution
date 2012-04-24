64708ea8da2bdea657bf7977003d8a9e6b015bbe
Matthew Strickland <mpstrick@gmail.com>
2012-04-24 14:15:10 -0400

    Fix popover and some site css.

---
+++ public/assets/js/application.js
@@ -0,0 +1,18 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+!function ($) {
+
+  $(function(){
+
+    // popover demo
+    $("a[rel=popover]")
+      .popover()
+      .click(function(e) {
+        e.preventDefault()
+      })
+
+  })
+
+}(window.jQuery)
